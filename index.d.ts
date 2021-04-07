285d001b0f1f34cfd81e89a33b232757e099d6f6
Isaac Mills <rooster0055@protonmail.com>
2021-04-07 09:26:56 -0400

    update types

--- index.d.ts
+++ index.d.ts
@@ -4,21 +4,21 @@ declare module "globkey" {
      */
     export function start(): void;
     /**
-     * Unloads and stops the globkey library
+     * Unloads and stops the globkey library. Returns a string if the worker thread could not be stopped
      */
-    export function unload(): void;
+    export function unload(): void | string;
     /**
-     * @returns an array of the currently pressed keys, or a boolean of false if it failed.
+     * @returns an array of the currently pressed keys, or a string containing an error if it failed.
      * @remarks this method will only fucntion properly if the `start()` function has already been called
      */
-    export function getKeys(): string[] | boolean;
+    export function getKeys(): string[] | string;
     /**
      * @returns a boolean value based on if globkey is runnig or not.
      * @remarks this method will only return `true` if `start()` has been called
      */
     export function isRunning(): boolean;
     /**
-     * Unloads and stops the globkey library as well as kills the nodejs process
+     * Unloads and stops the globkey library as well as kills the nodejs process. Returns a string if the worker thread could not be stopped
      */
-    export function stop(): void;
+    export function stop(): void | string;
 }
